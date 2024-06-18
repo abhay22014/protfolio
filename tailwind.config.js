b5be03791b687d8c5345dc79ca700f1984a07233
abhay22014 <132477567+abhay22014@users.noreply.github.com>
2024-06-19 00:15:19 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./dist/*.html"],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+  }
+  /**
+   * run this command in terminal
+   npx tailwindcss -i ./src/input.css -o ./Dist/style.css --watch 
+   */
+  
+  
